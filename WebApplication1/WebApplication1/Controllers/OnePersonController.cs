ea0aaf42ba2aa8b70353f10a5f4cea05bc9a6ce4
Ayman Boulad <boulada952@gmail.com>
2023-11-07 20:34:46 -0600

    LandMark: last working copy of V2 prior to cert changes

--- WebApplication1/WebApplication1/Controllers/OnePersonController.cs
+++ WebApplication1/WebApplication1/Controllers/OnePersonController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using WebApplication1.DTO;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
@@ -18,7 +19,7 @@ namespace WebApplication1.Controllers
         }
 
 
-        [HttpGet("{firstName}")]
+        [HttpGet("ByFirstName/{firstName}")]
         public async Task<ActionResult<IEnumerable<Person>>> GetPeopleByFirstName(string firstName)
         {
             // Retrieve records from the "Person" table where the FirstName matches the provided value
@@ -33,6 +34,60 @@ namespace WebApplication1.Controllers
 
             return Ok(people); // Return a 200 OK response with the list of people matching the first name
         }
+
+        [HttpGet("ByLastName/{lastName}")]
+        public async Task<ActionResult<IEnumerable<Person>>> GetPeopleByLastName(string lastName)
+        {
+            // Retrieve records from the "Person" table where the FirstName matches the provided value
+            var people = await _context.People
+                .Where(p => p.LastName == lastName)
+                .ToListAsync();
+
+            if (people == null || people.Count == 0)
+            {
+                return NotFound(); // Return a 404 Not Found response if no records match the first name
+            }
+
+            return Ok(people); // Return a 200 OK response with the list of people matching the first name
+        }
+
+        // POST endpoint to create a new person
+        [HttpPost("CreatePerson")]
+        public async Task<ActionResult<Person>> CreatePerson([FromBody] CreatePersonDto personCreateDto)
+        {
+            if (personCreateDto == null)
+            {
+                return BadRequest("Invalid request body.");
+            }
+
+            // Create a new Person entity from the DTO
+            var newPerson = new Person
+            {
+                FirstName = personCreateDto.FirstName,
+                LastName = personCreateDto.LastName,
+                Email = personCreateDto.Email,
+                PhoneNumber = personCreateDto.PhoneNumber,
+                Age = personCreateDto.Age,
+                Height = personCreateDto.Height,
+                Weight = personCreateDto.Weight,
+                Birthday = personCreateDto.Birthday
+            };
+
+            try
+            {
+                // Add the new person to the database
+                _context.People.Add(newPerson);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction(nameof(CreatePerson), new { id = newPerson.PersonId }, newPerson);
+            }
+            catch (DbUpdateException)
+            {
+                // Handle database update error
+                return StatusCode(500, "An error occurred while creating the person.");
+            }
+        }
+
 
     }
 }
---
+++ WebApplication1/WebApplication1/DTO/CreatePersonDto.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication1.DTO
+{
+    public class CreatePersonDto
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public int? Age { get; set; }
+
+        public decimal? Height { get; set; }
+
+        public decimal? Weight { get; set; }
+
+        public DateOnly? Birthday { get; set; }
+    }
+
+}
